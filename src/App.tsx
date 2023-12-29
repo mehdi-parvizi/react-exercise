68c45a17cb5cb770ed18525d22b01f474af72b6d
Mehdi <aceparvizi@gmail.com>
2023-12-29 09:42:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import { useState } from "react";
 import Alert from "./components/Training/Alert";
 import MyButton from "./components/Training/ButtonComponent";
 import ListGroup from "./components/Training/ListGroup";
-import ImageTraining from "./components/Training/ImageTraining";
+// import ImageTraining from "./components/Training/ImageTraining";
 import Greeting from "./components/Training/Greeting";
 import Like from "./components/Training/likeButton";
 function App() {
   const [currentState, updateState] = useState(false);
-  const handleSelectedItem = (item: string) => console.log(item);
   const handleSelectedItemTwo = (item: string) => console.log(item);
   const name = "Bo";
   const items = [`Newyork`, `Tokyo`, `Milano`, `Roma`];
   return (
     <div className="App">
-      <ListGroup items={items} heading="Cities" onSelectedItemTwo={() => {}} />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectedItemTwo={handleSelectedItemTwo}
+      />
       <Greeting name={name} />
       {currentState && (
         <Alert onClose={() => updateState(false)}>
